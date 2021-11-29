65b66fb56a0b7f15514cc3c7e9161bb1e947e379
Aaron Woo <awoo@chapman.edu>
2021-11-29 19:13:56 +0000

    Refactored and solved areSimilar algo.

--- areSimilar.js
+++ areSimilar.js
@@ -1,84 +1,28 @@
 function solution(a, b) {
-  const aObjs = [];
-  const bObjs = [];
-  for (let i = 0; i < a.length; i++) {
-    aObjs.push({
-      num: a[i],
-      index: i
-    })
-    bObjs.push({
-      num: b[i],
-      index: i
-    })
-  }
-  let allEqual = true;
-  for (let i = 0; i < aObjs.length; i++) {
-    if (aObjs[i].num !== bObjs[i].num) {
-      allEqual = false;
+    const indicesToSwap = [];
+    for (let i = 0; i < a.length; i++) {
+      if (a[i] !== b[i]) {
+        indicesToSwap.push(i);
+      }
     }
-  }
-  if (allEqual) {
+
+    if (indicesToSwap.length === 0) {
+      return true;
+    }
+    if (indicesToSwap.length > 2) {
+      return false
+    }
+
+    const aCopy = a.slice();
+    const aFirstSwap = a[indicesToSwap[0]];
+    aCopy[indicesToSwap[0]] = a[indicesToSwap[1]];
+    aCopy[indicesToSwap[1]] = a[indicesToSwap[0]];
+    console.log('aCopy', aCopy);
+    console.log('b', b);
+    for (let i = 0; i < aCopy.length; i++) {
+      if (aCopy[i] !== b[i]) {
+        return false
+      }
+    }
     return true;
-  }
-
-  let aElem;
-  let bElem;
-  let aMatchIndex;
-  for (let i = 0; i < aObjs.length; i++) {
-    aElem = aObjs[i];
-    for (let j = 0; j < bObjs.length; j++) {
-      bElem = bObjs[j]
-      if (bElem.num === aElem.num && bElem.index !== aElem.index) {
-        aMatchIndex = aObjs[j];
-      }
-    }
-    if (!aMatchIndex) {
-      continue;
-    }
-    for (let k = 0; k < bObjs.length; k++) {
-      if (bObjs[k].num === aMatchIndex.num && bObjs[k].index === aElem.index) {
-        let allOtherEqual = true;
-        for (let i = 0; i < aObjs.length; i++) {
-          if (i === aElem.index || i === aMatchIndex.index) {
-            continue;
-          }
-          if (aObjs[i].num !== bObjs[i].num) {
-            allOtherEqual = false;
-          }
-        }
-        if (allOtherEqual) {
-          return true;
-        }
-      }
-    }
-    return false;
-  }
 }
-
-    // compare arrays that have the same numbers as well as instances of each number
-    // otherwise, return false, since there is no way to obtain an array of different numbers
-
-    // create an object for each array
-    // loop thru each array
-        // add a property to the object for each unique number in the array
-        // if the property exists in the array, increment its count
-    // compare each property in the objects
-    // if a property's value is different then return false
-    // otherwise, compare the arrays
-
-    // loop thru each array
-        // create an object for each element
-        // add properties for the element's number and index
-
-    // loop thru the array of objects for a, index i
-        // const element = a[0]  num = 1.   index = 0
-        // loop thru array of objects for b, index j
-            // if b[j].num === a[0].num = 1         -checking array b where the number is the same
-                // get a[j].num = 3                 -getting the element at the index, number is the same^
-        // loop thru array of objects for b, index k
-            // if b[k].num === a[j].num && b[k].index === a[0].index    -if the
-                // return true
-
-
-    // guard for same index and same number swap
-    // gaurd for arrays of differing number instances
